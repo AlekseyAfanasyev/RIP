d6ccbb18506af372c22a827343761a3a410703f1
AlekseyAfanasyev <Aleksey.afan1@yandex.ru>
2023-09-14 11:51:37 +0300

    bug fix

--- internal/api/server.go
+++ internal/api/server.go
@@ -105,7 +105,7 @@ func StartServer() {
 	err := r.Run(":8080")
 	if err != nil {
 		return
-	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	}
 
 	log.Println("Server down")
 }
